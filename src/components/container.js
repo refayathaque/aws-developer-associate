04c02cda30ee9863f65c7527df0ee9bd640c9388
Refayat Haque <refayathaque@gmail.com>
2018-08-20 14:08:32 -0400

    in progress

--- src/components/container.js
+++ src/components/container.js
@@ -4,13 +4,19 @@ import '../styles.css';
 import Header from './header.js';
 import Footer from './footer.js';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fab, fas, far)
+
 class Container extends Component {
     render() {
         return (
             <div className="container">
                 <div>
                     <Header />
-                    {/* <Footer /> */}
+                    <Footer />
                 </div>
             </div>
         );
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import '../styles.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Footer = () => {
 
     return (
         <div className='footer-container'>
-            <h3>The End</h3>
+            <h4>Made with &nbsp;
+                <FontAwesomeIcon icon={["far", "heart"]} />
+                &nbsp; using &nbsp;
+                <FontAwesomeIcon icon={["fab", "js"]} size="lg" />
+                &nbsp; and &nbsp;
+                <FontAwesomeIcon icon={["fab", "react"]} size="lg" />
+                &nbsp; Hosted in the &nbsp;
+                <FontAwesomeIcon icon={["fas", "cloud"]} />
+                &nbsp; with &nbsp;
+                <FontAwesomeIcon icon={["fab", "aws"]} size="lg" />
+                &nbsp; | &nbsp;
+                <a href="https://github.com/refayathaque/aws-developer-associate" target="_blank">
+                    <FontAwesomeIcon icon={["fab", "github"]} size="lg" />
+                </a>
+
+            </h4>
         </div>
     )
 }
