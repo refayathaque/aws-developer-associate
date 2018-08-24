3ba89c2f7657d9671e91877a91f8cfcd5e273e9e
Refayat Haque <refayathaque@gmail.com>
2018-08-23 20:12:03 -0400

    in progress

--- src/components/topics/cloudfront.js
+++ src/components/topics/cloudfront.js
@@ -16,7 +16,7 @@ const CloudFront = () => {
                     <li><i>Content Delivery Network</i></li>
                     <li>System of distributed servers (network) that deliver webpages and other web content to a user based on the geographic locations of the user, the origin of the webpage, and a content delivery server</li>
                     <li><strong>Speeds up the distribution</strong> of our static and dynamic web content (HTML, JavaScript, CSS, images)</li>
-                    <li>Edge Locations - Situated all over the world - CloudFront uses these to keep a <strong>cache of our web content</strong> (we can also write to it) - Resulting in a much faster response time - Dramatically reduces network hops for each request - <strong>Different from an AWS region/AZ</strong></li>
+                    <li>Edge Locations - Situated all over the world - CloudFront uses these to keep a <strong>cache of our web content</strong> (we can also write to it, i.e., <strong>PUT an object on to them</strong>, e.g., Transfer Acceleration) - Resulting in a much faster response time - Dramatically reduces network hops for each request - <strong>Different from an AWS region/AZ</strong></li>
                     <li>Origin - Of files the CDN will distribute, e.g., S3 bucket, EC2 instance, an Elastic Load Balancer (ELB), or Route53</li>
                     <li>Distribution - Name of the CDN, which consists of a collection of Edge Locations</li>
                     <li>Web Distribution - Typically used for websites, e.g., HTTP/HTTPS</li>
@@ -29,12 +29,14 @@ const CloudFront = () => {
                     <li>Can be used to deliver our entire website, including dynamic, static, streaming, and interactive content using a global network of edge location</li>
                     <li>Requests for your content are <strong>automatically routed to the nearest edge location</strong>, so content is delivered with the best possible performance - E.g., can be used to optimize performance for users accessing a website backed by S3</li>
                     <li>Also works seamlessly with any <strong>non-AWS origin server</strong>, which stores the original, definitive versions of your files</li>
+                    <li>Objects are <strong>cached for the life of the TTL (Time To Live)</strong></li>
+                    <li>We can <strong>clear cached objects at any time, but we will be charged</strong> for it</li>
                 </ul>
             </div>
             <div className="topics-flex-item-inline">
                 <h2>CloudFront and S3 Transfer Acceleration</h2>
                 <ul>
-                    <li>Amazon S3 Transfer Acceleration enables fast, easy, and secure transfers of files <i>over long distances</i> between your end usrs and an S3 bucket</li>
+                    <li>Amazon S3 Transfer Acceleration enables fast, easy, and secure transfers of files <i>over long distances</i> between your end usrs and an S3 bucket - Reduced latency for S3 uploads</li>
                     <li>Transfer Acceleration - Takes advantage of CloudFront's globally distributed edge locations - As the data arrives at an edge location, data is routed to S3 over an <strong>optimized network path</strong> (essentially these are fast networks <strong>within</strong> AWS)</li>
                 </ul>
             </div>
