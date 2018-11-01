e0a5be12274c249babc91e179750bc29600f6303
Refayat Haque <refayathaque@gmail.com>
2018-11-01 17:24:32 -0400

    committing/pushing code and deploying to S3

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <div className='header-container'>
             <h1>AWS Developer Associate</h1>
+            <h3>{process.env.REACT_APP_SECRET_CODE}</h3>
             <h2><i>Released June 2018</i></h2>
         </div>
     )
